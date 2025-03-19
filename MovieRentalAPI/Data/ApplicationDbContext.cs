0f457813a1c1ddb039dc515d7cf69c85380f7dc4
qxz1s0l <Piotr.Nippe@partner.bmw.de>
2025-03-19 06:59:58 +0100

    API without test

---
+++ MovieRentalAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿namespace MovieRentalAPI.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MovieRentalAPI.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookStatus> BookStatuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BookStatus>().HasData(
+                new BookStatus { Id = 1, Name = "Na półce" },
+                new BookStatus { Id = 2, Name = "Wypożyczona" },
+                new BookStatus { Id = 3, Name = "Zwrócona" },
+                new BookStatus { Id = 4, Name = "Uszkodzona" }
+            );
+
+            //Example data  TODO: remove
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id = 1, Title = "Hobbit", Author="Tolkien", ISBN= "9780345445605", BookStatusId=1 },
+                new Book { Id = 2, Title = "LotR: Drużyna pierścienia", Author = "Tolkien", ISBN = "9780345445606", BookStatusId = 2 },
+                new Book { Id = 3, Title = "LotR: Dwie wieże", Author = "Tolkien", ISBN = "9780345445607", BookStatusId = 3 },
+                new Book { Id = 4, Title = "Lotr: Powrót Króla", Author = "Tolkien", ISBN = "9780345445608", BookStatusId = 4 }
+            );
+
+            modelBuilder.Entity<Book>()
+                    .HasIndex(b => b.ISBN)
+                    .IsUnique();
+        }
+    }
+}
---
+++ MovieRentalAPI/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿namespace MovieRentalAPI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public required string Title { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public required string Author { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{13}$", ErrorMessage = "ISBN must be a 13-digit number.")]
+        public required string ISBN { get; set; }
+
+        [Required]
+        public int BookStatusId { get; set; }
+
+        [ForeignKey("BookStatusId")]
+        public BookStatus BookStatus { get; set; }
+    }
+}
